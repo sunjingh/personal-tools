c74f20c5a402de6393c8f948e6b848fc0aff8b63
sunjinghao <365033374@qq.com>
2024-12-13 16:21:28 +0800

    build(vite): 配置服务器主机和端口
    - 在 vite.config.ts 中添加 server 配置
    - 设置 host 为 true，允许外部访问
    - 设置 port 为 8081，指定开发服务器端口

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 8081,
+  },
   plugins: [
     vue(),
     vueDevTools(),
